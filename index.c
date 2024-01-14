d9e53248bf5b9316117d1f43dc2f32a956b4b7d9
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-14 17:45:26 +0300

    it was a such a silly mistake too jesus fucking christ

--- index.c
+++ index.c
@@ -11,8 +11,8 @@ typedef struct
 void main(){
 
     User user1 = {"Mesfin", '@', 19, 34000.50};
-    printf("%s", user1.age);
-    printf("%c", user1.something);
-    printf("%i", user1.age);
-    printf("%lf", user1.salary);
+    printf("%s \n", user1.name);
+    printf("%c \n", user1.something);
+    printf("%i \n", user1.age);
+    printf("%.2lf \n", user1.salary);
 }
