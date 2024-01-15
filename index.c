3bee7528961e326d4a496c984cb9c6e5659d5f7a
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-15 20:50:59 +0300

    I ahve no fucking clue why there's an error. i miss python.

--- index.c
+++ index.c
@@ -1,18 +1,40 @@
+//I have no idea what structs do, but I also kinda do
 #include <stdio.h>
 
-typedef struct
-{
-    char name[24];
-    char something;
-    int age;
-    double salary;
-} User;
+typedef struct {
 
-void main(){
+    int damage;
+    char type[2];
+    char damagetype[4];
 
-    User user1 = {"Mesfin", '@', 19, 34000.50};
-    printf("%s \n", user1.name);
-    printf("%c \n", user1.something);
-    printf("%i \n", user1.age);
-    printf("%.2lf \n", user1.salary);
+}BulletTypes;
+
+BulletTypes incendiary = {50, "INC", "FIRE"};
+BulletTypes buckshot = {40, "BCK", "FIRE"};
+BulletTypes duckshot = {5, "DCK", "FIRE"};
+
+int main(){
+
+    int o = 0;
+    char response;
+
+    printf("Are you ready to play the game?");
+    scanf(">>> %c", &response);
+
+    if(response = 'Y'){
+        printf("You said yes");
+        o ++;
+    }
+    else if (response = 'N')
+    {
+        printf("You said no");
+        o++;
+    }
+
+    else{
+        printf("Please input a valid response");
+    }
+    
+
+    return 0;
 }
