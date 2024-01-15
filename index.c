14ec7b37a69c49d6bcf282a6a24eb783b7310a47
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-15 20:54:15 +0300

    ...

--- index.c
+++ index.c
@@ -1,40 +1,7 @@
-//I have no idea what structs do, but I also kinda do
 #include <stdio.h>
-
-typedef struct {
-
-    int damage;
-    char type[2];
-    char damagetype[4];
-
-}BulletTypes;
-
-BulletTypes incendiary = {50, "INC", "FIRE"};
-BulletTypes buckshot = {40, "BCK", "FIRE"};
-BulletTypes duckshot = {5, "DCK", "FIRE"};
 
 int main(){
 
-    int o = 0;
-    char response;
-
-    printf("Are you ready to play the game?");
-    scanf(">>> %c", &response);
-
-    if(response = 'Y'){
-        printf("You said yes");
-        o ++;
-    }
-    else if (response = 'N')
-    {
-        printf("You said no");
-        o++;
-    }
-
-    else{
-        printf("Please input a valid response");
-    }
-    
-
+    printf("Man fuck this shit I'm jerking off!");
     return 0;
 }
