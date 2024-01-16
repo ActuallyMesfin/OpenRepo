06a05df5ef3bde4c95a5ce6b2ffc79aecdb16ddc
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-16 19:35:54 +0300

    What the fuck? What. The. Fuck?

--- index.c
+++ index.c
@@ -2,18 +2,25 @@
 
 int main(){
 
-    int array[3][3] = {{0,1,2}, 
-                       {3,4,5}, 
-                       {6,7,8}};
+    /*char symbol;
+    int height;
 
-    /*
-    So based on my silly understanding, take the location [x][y]
-    x states the row? And y the column?
-    When will this ever be applicable anyway? Like making a grid of zeros to represent
-    black or some shit like that? I ain't doin allat gang
+    printf("Symbol >>> ");
+    scanf("%c", &symbol);
+
+    printf("Height >>> ");
+    scanf("%i", &height);
     */
+    for(int i; i <= 5; i++){
 
-    printf("%i", array[2][1]);
+        printf("$\n");
+
+        for(int j; j < i; j++){
+
+            printf("$");
+
+        }
+    }
 
     return 0;
 }
