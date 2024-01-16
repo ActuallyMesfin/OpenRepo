98f4d3cc01c3c46f0795487e3dbe993b874a284b
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-16 18:11:29 +0300

    Bruh

--- index.c
+++ index.c
@@ -2,6 +2,18 @@
 
 int main(){
 
-    printf("Man fuck this shit I'm jerking off!");
+    int array[3][3] = {{0,1,2}, 
+                       {3,4,5}, 
+                       {6,7,8}};
+
+    /*
+    So based on my silly understanding, take the location [x][y]
+    x states the row? And y the column?
+    When will this ever be applicable anyway? Like making a grid of zeros to represent
+    black or some shit like that? I ain't doin allat gang
+    */
+
+    printf("%i", array[2][1]);
+
     return 0;
 }
