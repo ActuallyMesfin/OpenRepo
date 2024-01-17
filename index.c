90f238086891e02188ca2c97cba2b2a8126b0bb4
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-17 19:55:18 +0300

    lol

--- index.c
+++ index.c
@@ -2,25 +2,7 @@
 
 int main(){
 
-    /*char symbol;
-    int height;
-
-    printf("Symbol >>> ");
-    scanf("%c", &symbol);
-
-    printf("Height >>> ");
-    scanf("%i", &height);
-    */
-    for(int i; i <= 5; i++){
-
-        printf("$\n");
-
-        for(int j; j < i; j++){
-
-            printf("$");
-
-        }
-    }
+    printf("Yes I'm cheating");
 
     return 0;
 }
