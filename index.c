b597f41ae486b99e124cd18e2989b37ca1967921
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-22 20:24:06 +0300

    I was just trynna see how many bytes letters have. Guess they're all 1 byte big.

--- index.c
+++ index.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+int main(){
+
+    char list[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (int i = 0; i < sizeof(list); i++)
+    {
+        printf("%i \n", sizeof(list[i]));
+    }
+    
+    
+
+    return 0;
+}
