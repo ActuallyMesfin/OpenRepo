776dea012a2b52e7e29dc0bd411352741d223f8c
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-27 20:42:01 +0300

    YUH

--- index.c
+++ index.c
@@ -1,1 +1,7 @@
-//I learned the basics. What now? I'm so lost I'm genuinely tweakin out
+//Here's what I'm gonna do. Leetcode. I'll practice there and then come back. I'll be the best I promise
+int main(){
+
+    printf("I'll be back");
+
+    return 0;
+}
