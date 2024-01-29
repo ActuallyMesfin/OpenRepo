c1dbec8e3de5c47a073541b6f26cab65f2785958
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-29 18:26:31 +0300

    ayo?

--- index.c
+++ index.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <time.h>
-#include <stdlib.h>
 
 int main(){
 
-    int length;
-    srand(time(0));
-
-    printf("How long do you want this shit to take?");
-
-    scanf("%d", &length);
-
-    for(int i; i <= length * 2; i++){
-        
-        int timelength = rand() % length;
-        printf("#");
-        sleep(timelength);
-
-    }
-
-    return 0;
+    return 1;
+    
 }
