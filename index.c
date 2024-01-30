74c62e9c772a93d3de2928d98366392cba041a95
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-30 21:25:08 +0300

    I really was excited to make this. Should be an easy fix. But I wanna play Far Cry 4 now :(

--- index.c
+++ index.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int * hexforint(int value){
+    return("%p", &value);
+}
 
 int main(){
 
-    char * name = malloc(4);
+    int user_input = 3;
+    //scanf("%i", &user_input);
 
-    name[0] = 'A';
-    name[1] = 'B';
-    name[2] = 'E';
-    name[3] = 'L';
-
-    /*printf("%p \n", &name[0]);
-    printf("%p \n", &name[1]);
-    printf("%p \n", &name[2]);
-    printf("%p", &name[3]);
-    */
-   for(int i; i < sizeof(name); i++){
-    printf("%c", name[i]);
-   }
-
-    free(name);
+    printf("%p", hexforint(user_input));
 
     return 0;
 }
