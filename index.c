3f4d21c609ce02969f84dd8de2d75fd756f8e789
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-30 21:15:06 +0300

    It is printing shit I've never seen in my life, but it kinda works, so meh. ALASKA!

--- index.c
+++ index.c
@@ -10,10 +10,14 @@ int main(){
     name[2] = 'E';
     name[3] = 'L';
 
-    printf("%p \n", &name[0]);
+    /*printf("%p \n", &name[0]);
     printf("%p \n", &name[1]);
     printf("%p \n", &name[2]);
     printf("%p", &name[3]);
+    */
+   for(int i; i < sizeof(name); i++){
+    printf("%c", name[i]);
+   }
 
     free(name);
 
