d6d8db9f4f5f9041004dfb5d995f996518d934c6
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-01-31 19:54:56 +0300

    January is OVER

--- index.c
+++ index.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
-
-int * hexforint(int value){
-    return("%p", &value);
-}
+//there's a nation wide power problem rn. The lights are flickering. Let me get a quick function in rq
+//I just realized after finishing this function that I didn't even need to include <stdio.h>? tf? 
+double sum();
 
 int main(){
 
-    int user_input = 3;
-    //scanf("%i", &user_input);
-
-    printf("%p", hexforint(user_input));
+    printf("%.1lf", sum(1, 2));
 
     return 0;
+}
+
+double sum(int number1, int number2){
+    return number1 + number2;
 }
