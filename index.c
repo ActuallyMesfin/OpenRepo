a88d703dccba926c93e173cda6470f5241988dfa
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-05 17:27:18 +0300

    Yes

--- index.c
+++ index.c
@@ -4,14 +4,15 @@ main(){
 
     float celc, fahr, jump;
 
-    celc = 0;
     fahr = 0;
     jump = 5;
 
     while(fahr <= 50){
+
         celc = 5 * (fahr - 32) / 9;
-        printf("%.2f \t %.2f \n", fahr, celc);
+        printf("%.1f \t %.1f \n", fahr, celc);
         fahr += jump;
+        
     }
 
     return 0;
