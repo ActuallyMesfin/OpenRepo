dc4d7397f92ed57767c521d8984ea79a538ac22c
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-05 17:24:57 +0300

    Dennis Ritchie LOL

--- index.c
+++ index.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-int main(){
-    //I downloaded the C Programming Language Book. Will I suffer? I need to learn more!
+main(){
+
+    float celc, fahr, jump;
+
+    celc = 0;
+    fahr = 0;
+    jump = 5;
+
+    while(fahr <= 50){
+        celc = 5 * (fahr - 32) / 9;
+        printf("%.2f \t %.2f \n", fahr, celc);
+        fahr += jump;
+    }
+
+    return 0;
 }
