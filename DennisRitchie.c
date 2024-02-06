449309f2c342a48fcc3281cad02f0c46424ec81a
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-06 21:01:47 +0300

    That book is wonderful ngl

---
+++ DennisRitchie.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    #define variable "HELLO" //So THIS is what define statements do
+    #define dennisritchie 5 //Because I Love Dennis Ritchie
+
+    printf("%s\n", variable);
+    printf("%d", dennisritchie);
+
+    return 0;
+}
