7761795030b7b795c1d77744009cdab4a07b14f7
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-07 18:23:13 +0300

    :3

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main(){
 
-    #define variable "HELLO" //So THIS is what define statements do
-    #define dennisritchie 5 //Because I Love Dennis Ritchie
-
-    printf("%s\n", variable);
-    printf("%d", dennisritchie);
-
-    return 0;
+int main()
+{
+    char x = getchar();
+    putchar(x);
+    
+    //so a primitive printf(x[0])? I can't use printf tho.
 }
