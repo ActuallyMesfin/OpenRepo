0c262a01d5a8c70073d9b36b79d32b70efe11c02
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-10 19:23:04 +0300

    Fanum done taxed bro's income😭

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,8 +1,20 @@
-//I'm playing tf2 while coding lel
-//listening to joe rogan *moans*
 #include <stdio.h>
 
-main(){
-    printf("AHHHHHHHHHHHHHHHHh");
+double vat(double income);
+double insurance(double income);
+
+int main(){
+    double income = 50000;
+    income = vat(income);
+    income = insurance(income);
+    printf("Your final income is : %lf", income);
     return 0;
+}
+
+double vat(double income){
+    return ((income * 15) / 100);
+}
+
+double insurance(double income){
+    return (income - ((income * 5)/100));
 }
