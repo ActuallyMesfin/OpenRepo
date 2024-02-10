b7440afe3e9e7d93d7c15aa686568c0a3aa8983a
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-10 19:09:26 +0300

    YEAHHHH

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,25 +1,8 @@
-//FUNCTIONS
-/*return type(arguments){
-    declarations;
-    statements;    
-}*/
-void function1();
-double function2();
+//I'm playing tf2 while coding lel
+//listening to joe rogan *moans*
+#include <stdio.h>
 
 main(){
-    function1();
-    int c = function2(2,2);
-    printf("%d", c);
+    printf("AHHHHHHHHHHHHHHHHh");
     return 0;
-}
-
-
-
-void function1(){
-    printf("Hello World\n");
-    return 0;
-}
-
-double function2(int number1, int number2){
-    return (number1 * number2);
 }
