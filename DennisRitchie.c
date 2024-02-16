b3108e9abc3b30b4195ba4588c2395f61232274e
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-16 14:43:03 +0300

    Boys will look at this and say hell yeah

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
+#include <math.h>
+
+double pythagoras(float opposite, float adjacent);
+
 int main(){
-	int var[10];
-	var[0] = 1;
-	var[1] = 2;
-	int *pVar;
-	pVar = &var[1];
-	printf("%d", *pVar);
+	printf("%f\n", pythagoras(3,4));
 	return 0;
 }
+double pythagoras(float opposite, float adjacent){
+	float hypotenuse = sqrt(pow(opposite, 2) + pow(adjacent, 2));
+	return hypotenuse;
+}
+				/*
+
+						 /| 	
+						/ |
+					   /  |
+		  Hypotenuse  /	  |	  	  	(Hypotenuse ^ 2) = (Opposite ^ 2) + (Adjacent ^ 2)
+					 /	  | Opposite
+					/	  |
+				   /	  | 	
+				  /		  | 	
+				 /		 _| 	
+				/_______|_|	<--90 Deg
+
+				Adjacent
+There was absolutely no reason for me to draw this thing but it looks so good I don't regret it
+				*/
