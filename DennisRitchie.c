081602c6aaa203ec8c9249a9294ac551b0fb2522
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-19 21:19:30 +0300

    Let me tell you something annoying about this. I wrote this exact same code. It didn't work. I deleted all of it and wrote it again. It worked. Now I'm crying listening to Joe Rogan talk about sex dolls. W

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-//experimentation
-int main(){
-	int numbers[6] = {1, 2, 3, 5, 10, 20};
-	for(int i = 0; i <=6; i++){
-		printf("%p\n", &numbers[i]);
-		//it looks like there's a pattern for the memory addresses. idk.
-	}
-	return 0;
+#include <string.h>
+struct User {
+  int age;
+  char name[10];
+};
+
+int main() {
+  struct User nigga = {16, "Alan"};
+  printf("%s is age %d", nigga.name, nigga.age);
 }
