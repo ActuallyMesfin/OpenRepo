08166eb2724884c1813611d8e38dce6b49b989a6
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-02-20 19:00:38 +0300

    enums

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-struct User {
-  int age;
-  char name[10];
+enum level{
+  FIRST,
+  SECOND,
+  THIRD
 };
-
-int main() {
-  struct User nigga = {16, "Alan"};
-  printf("%s is age %d", nigga.name, nigga.age);
+main(){
+  enum level variable = FIRST;
+  printf("%d", variable);
+  return 0;
 }
