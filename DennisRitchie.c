dc8f668c3bed69e39b15462f6d4375e9aa890906
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-06 20:19:51 +0300

    Fuck's sake

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <time.h>
-#include <random>
+
 
 
 struct loadout
 {
-  char weapon_name[3];
-  char player_class[3];
+  char weapon_name[10];
+  char player_class[10];
 };
 
 
 int main(){
-  
+  struct loadout weapon1 = {"Scattergun", "Scout"};
+  printf("%s", weapon1.player_class);
   return 0;
 }
