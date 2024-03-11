f1fa732a149c7885da3e27828875754ede3c9a67
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-11 21:52:14 +0300

    I failed maths today F

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 
-
-
-struct loadout
-{
-  char weapon_name[10];
-  char player_class[10];
-};
-
-
 int main(){
-  struct loadout weapon1 = {"Scattergun", "Scout"};
-  printf("%s", weapon1.player_class);
+  printf("Compiler is fucking shit");
   return 0;
 }
