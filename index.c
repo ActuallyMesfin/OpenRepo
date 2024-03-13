37d06b3b165b602e84ee7f03b0c9063100ee4cf7
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-13 20:36:27 +0300

    Oh my god I fucking get it

--- index.c
+++ index.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 
-main(){
-
-    char hello[] = "Hello";
-
-    for(int i = 0; i <= sizeof(hello) - 2; i++){
-        printf("%c \t %p \n", hello[i], &hello[i]);
-    }
-    //where did that two come from? Why are there two null lines that take up 2 bytes of RAM?
-    //Is this what they meant when they said C is death incarnate? >_<
+int main(){
+    int value;
+    int* ptr = &value;
+    printf("%p\n", &value);
+    printf("%p", ptr);
     return 0;
 }
-//fuck everyone 
-//yeah you know what imma make a function
-//that says that for me
-
-void fuck_everybody(){
-
-        printf("Fuck Everybody");
-}
-//man I hate this
