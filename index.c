a24dabe577e6a7e6917c380d730981e1ea5b44aa
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-16 18:13:42 +0300

    C

--- index.c
+++ index.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
 int main(){
-    int values[3][3] = {{1}, {2}, {3}};
-
-    for(int i = 0; i <= sizeof(values); i++){
-        printf("%p\n", &values);
-    }
-    return 0;
+    char format[13] = "XXXX-XXX-XXXX"; 
 }
