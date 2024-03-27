788cc3df38a84bc52f8d69a019f5c14c2cba4ae9
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-27 14:49:41 +0300

    Addresses of values in a matrix

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,10 +1,20 @@
 //refresher on pointers
+//pointer at 3d array
 #include <stdio.h>
 
 int main(){
-  int values[3] = {1, 2, 3};
-  int *ptr = &values[0];
-  printf("Value at Index [0] : %d\n", values[0]);
-  printf("Value of Pointer : %d\n", *ptr);
-  printf("Address of Value : %p\n", &values[0]);
+  int values[3][3] = {{1, 2, 3},
+                     {4, 5, 6},
+                     {7, 8, 9}};
+/*
+  [0,0] [0,1] [0,2]
+  [1,0] [1,1] [1,2]
+  [2,0] [2,1] [2,2]
+*/
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      printf("Address at Point[%d][%d] : %p \n", i, j, &values[i][j]);
+    }
+  }
+  return 0;
 }
