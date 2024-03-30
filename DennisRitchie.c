16ff311c07772a0432ada58c1d4e3e4e3715c808
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-03-30 20:10:39 +0300

    Coo

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,20 +1,8 @@
-//refresher on pointers
-//pointer at 3d array
 #include <stdio.h>
 
 int main(){
-  int values[3][3] = {{1, 2, 3},
-                     {4, 5, 6},
-                     {7, 8, 9}};
-/*
-  [0,0] [0,1] [0,2]
-  [1,0] [1,1] [1,2]
-  [2,0] [2,1] [2,2]
-*/
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
-      printf("Address at Point[%d][%d] : %p \n", i, j, &values[i][j]);
-    }
-  }
+  int value;
+  int* pValue = &value;
+  printf("%p", pValue);//needs a value I guess. Was cool to see though
   return 0;
 }
