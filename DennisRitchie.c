b38aef609dc39f6a58e731e6c52a9d41cc7a1111
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-04-05 17:46:01 +0300

    YUH🪛

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,19 +1,11 @@
+//typecasting
 #include <stdio.h>
-
-void swap(int *a, int *b);
 
 int main(){
     int a = 5;
-    int b = 6;
-    printf("a: %d\nb: %d\n", a, b);
-    swap(&a, &b);
-    printf("a: %d\nb: %d\n", a, b);
+    int b = 2;
+    printf("%d / %d = %d\n", a, b, a/b);
+    double c = (double) a / b;
+    printf("%d / %d = %f", a, b, c);
     return 0;
-}
-
-void swap(int *a, int *b){
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
 }
