eb079796315760fec6a1e2ab134256fb48192b42
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-04-09 14:21:07 +0300

    Calculator first try :3

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
+int add(int num1, int num2);
+int subtract(int num1, int num2);
+int divide(int num1, int num2);
+int multiply(int num1, int num2);
+
 int main(){
-    int a[3][3];
-    a[0][0] = 0;
-    int *b = &a[0][0];
-    printf("%d\n", a[0][0]);
-    printf("%p", a);
+    int a = 5;
+    int b = 5;
+    printf("Sum : %d\n", add(a, b));
+    printf("Difference : %d\n", subtract(a, b));
+    printf("Quotient : %d\n", divide(a, b));
+    printf("Product : %d\n", multiply(a, b));
     return 0;
-    //me big sad today
-    //man I have depression I swear
+}
+
+int add(int num1, int num2){
+    return num1 + num2;
+}
+int subtract(int num1, int num2){
+    return num1 - num2;
+}
+int divide(int num1, int num2){
+    return num1 / num2;
+}
+int multiply(int num1, int num2){
+    return num1 * num2;
 }
