172c4e762acc7bfeebf9e4b8e62e6f0389abf02b
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-04-12 18:41:22 +0300

    typedef

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,30 +1,10 @@
 #include <stdio.h>
-//RAM
-float add(float num1, float num2);
-float subtract(float num1, float num2);
-float divide(float num1, float num2);
-float multiply(float num1, float num2);
+
+typedef int i;
 
 int main(){
-    float a = 5;
-    float b = 5;
-    printf("Sum : %.2f\n", add(a, b));
-    printf("Difference : %.2f\n", subtract(a, b));
-    printf("Quotient : %.2f\n", divide(a, b));
-    printf("Product : %.2f\n", multiply(a, b));
-    printf("RAM taken: %d Bytes", sizeof(a) + sizeof(b));
+    i a = 5;
+    printf("%d", a);
+    //interesting but where to apply it?
     return 0;
-}
-
-float add(float num1, float num2){
-    return num1 + num2;
-}
-float subtract(float num1, float num2){
-    return num1 - num2;
-}
-float divide(float num1, float num2){
-    return num1 / num2;
-}
-float multiply(float num1, float num2){
-    return num1 * num2;
 }
