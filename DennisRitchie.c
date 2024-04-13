f97651116d09806434f2f998cc6b131709cc5a1b
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-04-13 19:31:02 +0300

    F

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-typedef int i;
-
 int main(){
-    i a = 5;
-    printf("%d", a);
-    //interesting but where to apply it?
+    printf("I'm running out of ideas");
     return 0;
 }
