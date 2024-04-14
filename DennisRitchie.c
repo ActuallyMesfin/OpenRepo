a947a96b340ec7499695522929e1831c0d77cc1a
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-04-14 16:10:21 +0300

    NULL

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    printf("I'm running out of ideas");
+    int *ptr = NULL;
+    int a;
+    printf("Address of null ptr:%p\n", ptr);
+    printf("Address of A: %p\n", &a);
+    ptr = &a;
+    printf("Address of null ptr:%p\n", ptr);
+    printf("Address of A: %p\n", &a);
     return 0;
 }
