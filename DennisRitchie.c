fdb186ce90fd7cef72e6c830f040c00645da583e
Mesfin Alemayehu <mesfinalemayehu435@gmail.com>
2024-04-15 20:15:26 +0300

    real

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -3,14 +3,14 @@
 
 int main() {
 
-    int *ptr;
-    ptr = (int *)malloc(sizeof(int));
+    void *ptr;
+    ptr = malloc(sizeof(int));
     if (ptr == NULL){
         printf("FAILED MEMORY ALLOCATION");
         return 1;
     }
     printf("%p\n", ptr);
-    printf("%d", sizeof(*ptr));
+    //printf("%d", sizeof(*ptr));
     free(ptr);
 
     return 0;
