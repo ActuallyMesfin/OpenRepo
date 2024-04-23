0df97f5599956e6847e6760838ce8541d3cdb9fd
ActuallyMesfin <mesfinalemayehu435@gmail.com>
2024-04-23 20:10:51 +0300

    idk anymore man

--- DennisRitchie.c
+++ DennisRitchie.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node
 {
     int data;
-    struct Node *ptr;
+    struct Node *next;
 };
+
+struct Node *head = NULL;
+struct Node *first = NULL;   
+struct Node *tail = NULL;
+
+one = malloc(sizeof(struct Node));
