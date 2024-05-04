05b064bb0254ceea86d76b284137c2d35eb2ba2a
ActuallyMesfin <mesfinalemayehu435@gmail.com>
2024-05-05 00:56:40 +0300

    I thik I gets it idk I was busy whole day (not)

---
+++ BinarySearch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int elements = sizeof(array)/sizeof(array[0]);
+    int target_value = 3;
+
+    for(int i = elements/2; i <= elements; i++){
+        if(array[i] > target_value){
+            printf("Target below Index[%d]\n", i);
+            i = i * 2;
+        }
+        elif(array[i] < target_value){
+            printf("Target above Index[%d]\n", i);
+            i = i * 2;
+        }
+        else{
+            printf("%d\n", array[i]);
+        }
+    }
+
+    return 0;
+}
