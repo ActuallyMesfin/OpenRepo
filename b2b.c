f86f65aea686332dd92a0c1853e3eff20bbdf1b5
ActuallyMesfin <mesfinalemayehu435@gmail.com>
2024-06-14 16:24:49 +0300

    lel

--- b2b.c
+++ b2b.c
@@ -1,36 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define Gravity 9.8
-#define Pi 180
-
-
-struct Pendulum
-{
-    
-    float mass;
-    float length;
-    float angle;
-
-};
-
-
-float period(){
-
-    struct Pendulum mass1; 
-
-    printf("Enter Mass : ");
-    scanf("%f\n", &mass1.mass);
-
-    printf("Enter Length : ");
-    scanf("%f\n", &mass1.length);
-
-    float lengthovergravity = mass1.length/Gravity;
-    float period = (2 * Pi) * sqrt(lengthovergravity);
-    return period;
-}
-
 int main(){
-    printf("The period of the pendulum is %f\n", period());
+    printf("%f", log(4));
     return 0;
 }
